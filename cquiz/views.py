4435633a7dc0cccb0ccb4115e4b4be104776c9fb
Moses Lagoon <moseslagoon@Mosess-MacBook-Pro.local>
2016-09-01 10:18:24 -0400

    Created Bootstrap templates. Ready to roll

--- cquiz/views.py
+++ cquiz/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("WELCOME TO CQUIZ's INDEX PAGE.")
+    context = {'do_it': 'cQuized', 'my_pages': ['Do 1', 'Do 2', 'Do 3', 'Do 4']}
+    return render(request, 'cQuiz/index.html', context = context)
