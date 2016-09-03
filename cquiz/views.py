cde36e5273fa73ded96522d2ce9e07d1cf6098e4
Moses Lagoon <moseslagoon@Mosess-MacBook-Pro.local>
2016-09-03 10:40:13 -0400

    Just updated the models database

--- cquiz/views.py
+++ cquiz/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    context = {'do_it': 'cQuized', 'my_pages': ['Do 1', 'Do 2', 'Do 3', 'Do 4']}
+    context = {'do_it': 'cQuized', 'my_dropdowns': ['d1', 'd2', 'd3', 'd4'],
+               'my_pages': ['Page 1', 'Page 2', 'Page 3', 'Page 4']}
+
     return render(request, 'cQuiz/index.html', context = context)
