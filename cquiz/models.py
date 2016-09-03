335b72c17243238a37671578def6ae3e9efef7f6
Moses Lagoon <moseslagoon@Mosess-MacBook-Pro.local>
2016-09-03 13:25:38 -0400

    Updated the model with update questions and choice methods to bulk create objects for storage

--- cquiz/models.py
+++ cquiz/models.py
@@ -7,9 +7,10 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=1000)
     pub_date = models.DateTimeField('date published')
+    q_id = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.question_text
+        return "{0}. {1}".format(self.q_id, self.question_text)
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
@@ -18,6 +19,7 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=500)
     correct = models.BooleanField(default=True)
+    c_id = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text
+        return "{0} {1} {2} ".format(self.question.q_id, self.c_id, self.choice_text)
--- cquiz/update.py
+++ cquiz/update.py
@@ -1,16 +1,44 @@
+import os
 from django.utils import timezone
-import datetime
 from .models import Question, Choice
 
 def update():
-    filename = 'question.txt'
+    module_dir = os.path.dirname(__file__)
+    filename = os.path.join(module_dir, 'questions.txt')
     questions = []
     choices = []
     with open(filename) as my_file:
+        q_counter = 0
+        c_counter = 0
         for line in my_file:
             line = line.strip()
             if line.startswith('#'):
-                currQ = Question(question_text=line, pub_date=timezone.now())
+                line = line.replace('#', "")
+                q_counter += 1
+                c_counter=0
+                currQ = Question(question_text=line, pub_date=timezone.now(), q_id=q_counter)
                 questions.append(currQ)
             else:
-                ans = Choice(choice_text=line, correct=True, question=currQ)
+                c_counter += 1
+                ans = Choice(choice_text=line, correct=True, question=questions[len(questions)-1], c_id=c_counter)
+                choices.append(ans)
+    return questions, choices
+
+def  update_choice():
+    module_dir = os.path.dirname(__file__)
+    filename = os.path.join(module_dir, 'questions.txt')
+    questions = Question.objects.all()
+    answers = []
+    with open(filename) as myfile:
+        q_counter = 0
+        c_counter = 0
+        for line in myfile:
+            line = line.strip()
+            if line.startswith("#"):
+                q_counter += 1
+                c_counter = 0
+            else:
+                c_counter += 1
+                answer = Choice(choice_text=line, correct=True, question=questions[q_counter-1], c_id=c_counter)
+                answers.append(answer)
+    return answers
