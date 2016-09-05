9d39328722de0e0d4d05ef92c987a2e6f86c078a
Moses Lagoon <moseslagoon@Mosess-MacBook-Pro.local>
2016-09-05 11:08:36 -0400

    Added the modified files, made changes to html and database to display question and answers

--- cquiz/views.py
+++ cquiz/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question, Choice
 
 
 def index(request):
     context = {'do_it': 'cQuized', 'my_dropdowns': ['d1', 'd2', 'd3', 'd4'],
                'my_pages': ['Page 1', 'Page 2', 'Page 3', 'Page 4']}
 
-    return render(request, 'cQuiz/index.html', context = context)
+    return render(request, 'cQuiz/base.html', context = context)
+
+def qa(request):
+    question = Question.objects.all()
+    ans = Choice.objects.all()
+    context = {'do_it': 'cQuized', 'my_dropdowns': ['d1', 'd2', 'd3', 'd4'],
+               'my_pages': ['Page 1', 'Page 2', 'Page 3', 'Page 4'], 'question': question,
+               'ans': ans}
+    return render(request, 'cQuiz/form.html', context=context)
